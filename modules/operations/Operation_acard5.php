6d7f28430513a315637e80533b174c039f3e9983
VictoriaLa <laskava@gmail.com>
2023-11-30 18:27:36 -0500

    Bug #105370: "Search for life failed when revealing a microbe tag"

--- modules/operations/Operation_acard5.php
+++ modules/operations/Operation_acard5.php
@@ -18,11 +18,11 @@ class Operation_acard5 extends AbsOperation {
         //reveal and discard the top card of the draw deck. If that card has a microbe tag, add a science resource here
         $this->game->effect_moveCard($color, $card_id, "reveal", 0, $notif);
         $tags = $this->game->getRulesFor($card_id, 'tags', '');
-        if (strstr($tags, 'tagMicrobe')) {
-            $this->game->notifyMessage(clienttranslate('it has tag'));
+        if (strstr($tags, 'Microbe')) {
+            $this->game->notifyMessage(clienttranslate('it has a Microbe tag'));
             $this->game->putInEffectPool($color, "res", $this->getContext());
         } else {
-            $this->game->notifyMessage(clienttranslate('it does not have tag'));
+            $this->game->notifyMessage(clienttranslate('it does not have a Microbe tag'));
         }
         $this->game->effect_moveCard($color, $card_id, "discard_main", 0);
         return 1;
