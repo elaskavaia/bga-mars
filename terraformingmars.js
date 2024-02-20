e03072616a2a149bca1c5fe4d273645e80713865
benoit fleire <bfleire@gmail.com>
2024-02-20 18:03:05 +0100

    Upload js for deployment

--- terraformingmars.js
+++ terraformingmars.js
@@ -1317,7 +1317,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -3538,6 +3538,8 @@ var GameXBody = /** @class */ (function (_super) {
             var idx = 1;
             for (var key in pg) {
                 var pc = Math.ceil((pg[key] / goals[key]) * 100);
+                if (pc > 100)
+                    pc = 100;
                 var grade = "high";
                 if (pc <= 34)
                     grade = "low";
@@ -5716,7 +5718,7 @@ var GameXBody = /** @class */ (function (_super) {
         this.disableDragOnCard(tokenNode);
     };
     GameXBody.prototype.isManualSortOrderEnabled = function () {
-        if ($("hand_area").dataset.sort_type == "manual" && $("hand_area").dataset.sort_type == "increase") {
+        if ($("hand_area").dataset.sort_type == "manual" && $("hand_area").dataset.sort_direction == "increase") {
             return true;
         }
         else {
