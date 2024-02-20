b46e1219a912874c24a09d1e52b31451cf283665
VictoriaLa <laskava@gmail.com>
2024-02-19 19:28:58 -0500

    better fix for temp increase warning

--- modules/operations/Operation_convh.php
+++ modules/operations/Operation_convh.php
@@ -12,7 +12,7 @@ class Operation_convh extends AbsOperation {
 
     function effect(string $color, int $inc): int {
         $this->game->effect_incCount($color, 'h', -8);
-        $this->game->effect_increaseParam($color, 't', 1, 2);
+        $this->game->push($this->color,'t');
         return 1;
     }
 
@@ -23,29 +23,5 @@ class Operation_convh extends AbsOperation {
     }
     function getPrimaryArgType() {
         return 'token';
-    }
-
-    function isMax() {
-        $temp = $this->game->getTrackerValue('', 't');
-        if ($temp >= $this->getMax()) return true;
-        return false;
-    }
-
-    function requireConfirmation() {
-        if ($this->isMax()) return true;
-        return false;
-    }
-
-    function getPrompt() {
-        if ($this->isMax()) {
-            return clienttranslate('${you} must confirm, operation ${name} will not have effect as parameter is at max');
-        }
-
-        return parent::getPrompt();
-    }
-
-    function getMax() {
-        $max = $this->game->getRulesFor($this->game->getTrackerId('', 't'), 'max', 0);
-        return $max;
     }
 }
