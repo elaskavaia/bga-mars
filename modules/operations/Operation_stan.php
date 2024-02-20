38f693d401ae2fed067559b28930de61c7c0815f
VictoriaLa <laskava@gmail.com>
2024-02-19 23:00:26 -0500

    proper operation in pull for standard project

--- modules/operations/Operation_stan.php
+++ modules/operations/Operation_stan.php
@@ -8,10 +8,11 @@ class Operation_stan extends AbsOperation {
         $card_id = $this->getCheckedArg('target');
         $rules = $this->game->getRulesFor($card_id, 'r');
         $this->game->notifyMessageWithTokenName(clienttranslate('${player_name} plays standard project ${token_name}'), $card_id, $color);
+        $this->game->push($color, $this->game->getPayment($color, $card_id), $card_id);
         if ($card_id != 'card_stanproj_1')
             $this->game->notifyEffect($color, "play_stan", $card_id);
-        $this->game->push($color, $rules);
-        $this->game->push($color, $this->game->getPayment($color, $card_id), $card_id);
+        $this->game->putInEffectPool($color, $rules, $card_id);
+     
         return 1;
     }
 
