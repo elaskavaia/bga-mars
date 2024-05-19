08bf1775f0b15ffd63408d87941247680653b5c9
VictoriaLa <laskava@gmail.com>
2024-05-19 13:03:55 -0400

    fixed leftover confturn

--- modules/operations/Operation_confirm.php
+++ modules/operations/Operation_confirm.php
@@ -8,6 +8,14 @@ class Operation_confirm extends AbsOperation {
     }
 
     function canResolveAutomatically() {
+        $player_id = $this->getPlayerId();
+        if ($this->getMnemonic() == 'confturn') {
+            $pref = (int) $this->game->dbUserPrefs->getPrefValue($player_id, MA_PREF_CONFIRM_TURN);
+            if (!$pref) {
+                return true;
+            }
+        }
+
         return false;
     }
 
