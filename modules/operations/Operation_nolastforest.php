6dea27b91fc4043da09407a4a6ce5d4bc31314bc
VictoriaLa <laskava@gmail.com>
2025-04-03 17:24:11 -0400

    leave 1 plant in last forest action

--- modules/operations/Operation_nolastforest.php
+++ modules/operations/Operation_nolastforest.php
@@ -18,7 +18,9 @@ class Operation_nolastforest extends AbsOperation {
     function effect(string $color, int $inc, ?array $args = null): int {
         // when player denied placing last forest, remove the plants so it does not come and ask again
         $plants =  $this->game->getTrackerValue($color, 'p');
-        $this->game->effect_incCount($color, 'p', -$plants, ['message' => '']);
+        if ($plants > 1) {         // leave 1 plant it needed for award on amazonis
+            $this->game->effect_incCount($color, 'p', -$plants, ['message' => '']);
+        }
         return $inc;
     }
 
