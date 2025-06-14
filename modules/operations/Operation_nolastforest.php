d4e08c383484f1d65ca32a4af32583aa0bfcf06e
VictoriaLa <laskava@gmail.com>
2025-06-14 13:53:54 -0400

    #163326: "In final round rejecting greenery delete all plants"

--- modules/operations/Operation_nolastforest.php
+++ modules/operations/Operation_nolastforest.php
@@ -19,7 +19,7 @@ class Operation_nolastforest extends AbsOperation {
         // when player denied placing last forest, remove the plants so it does not come and ask again
         $plants =  $this->game->getTrackerValue($color, 'p');
         if ($plants > 1) {         // leave 1 plant it needed for award on amazonis
-            $this->game->effect_incCount($color, 'p', -$plants, ['message' => '']);
+            $this->game->effect_incCount($color, 'p', -$plants + 1, ['message' => '']);
         }
         return $inc;
     }
